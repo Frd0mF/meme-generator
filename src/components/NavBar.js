fb13ee8266d50051d137caf95e25f36338f58ddd
Frd0mF <81099546+Frd0mF@users.noreply.github.com>
2021-11-27 05:16:03 +0100

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+export default function navBar () {
+  return (
+    <nav className="navbar">
+        <img src="meme-face.png" alt="meme-face"/>
+        <h2>Meme Generator</h2>
+    </nav>
+  );
+}
